237fa962f16682a1ebc8080a8126b2fe300f9c27
Paul-dodot <johnpaul.daliva.97@gmail.com>
2025-03-24 11:33:00 +0800

    Function - Activity

---
+++ Activities/Function - Activity/script.js
@@ -0,0 +1,13 @@
+function WhatShallIwear(temp) {
+    if (temp < 60) {
+        console.log("Wear a jacket");
+    } else if (temp< 70) {
+        console.log("Wear a sweater");
+    } else {
+        console.log("wear a t-shirt")
+    }
+}
+
+WhatShallIwear(50);
+WhatShallIwear(60);
+WhatShallIwear(80);
